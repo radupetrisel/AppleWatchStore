07443419fc1eb5ed6b27628dab58f6d17ee1f420
Radu Petrisel <radupetrisel@gmail.com>
2023-10-03 20:47:57 +0300

    Initial Commit

---
+++ AppleWatchStore/AppleWatchStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchStoreApp.swift
+//  AppleWatchStore
+//
+//  Created by Radu Petrisel on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatchStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
